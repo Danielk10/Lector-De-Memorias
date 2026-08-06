babfff223b1e9bb1f4d72b98f367f48e2aaaf17e
Danielk10 <danielpdiamon@gmail.com>
2026-08-06 17:09:41 +0000

    Filter out Share dir and package name from terminal log & increment versionCode to 4

--- app/src/main/java/com/diamon/mini/core/MiniproExecutor.java
+++ app/src/main/java/com/diamon/mini/core/MiniproExecutor.java
@@ -120,7 +120,9 @@ public class MiniproExecutor {
                         new InputStreamReader(currentProcess.getInputStream()))) {
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        if (line.contains("Using overridden database file")) {
+                        if (line.contains("Using overridden database file")
+                                || line.contains("Share dir:")
+                                || line.contains("com.diamon.mini")) {
                             continue;
                         }
                         callback.log(line);
